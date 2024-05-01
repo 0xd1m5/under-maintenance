45540493c322bcab3eb3aba8afe10f07fc32e35a
syauqqii <dimas.syauqi007@gmail.com>
2024-05-01 10:24:50 +0700

    Feat: Change ID to EN

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,13 @@
 // Block "klik kanan"
 document.addEventListener('contextmenu', function (e) {
-    alert("Dalam Pembaruan, Harap Bersabar!");
+    alert("Under Maintenance");
     e.preventDefault();
 });
 
 // Block "CTRL + F"
 document.addEventListener('keydown', function (e) {
     if (e.ctrlKey && (e.key === 'f' || e.key === 'F')) {
-        alert("Dalam Pembaruan, Harap Bersabar!");
+        alert("Under Maintenance");
         e.preventDefault();
     }
 });
@@ -15,7 +15,7 @@ document.addEventListener('keydown', function (e) {
 // Block "CTRL + Shift + I"
 document.addEventListener('keydown', function (e) {
     if (e.ctrlKey && e.shiftKey && e.key === 'I') {
-        alert("Dalam Pembaruan, Harap Bersabar!");
+        alert("Under Maintenance");
         e.preventDefault();
     }
 });
